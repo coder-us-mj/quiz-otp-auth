54b2168adab9ed21f125605d856ae216e12139fd
coder-us-mj <coder.us.jm@gmail.com>
2025-07-11 18:17:16 +0530

    Add updated version

--- quiz_application/settings.py
+++ quiz_application/settings.py
@@ -13,7 +13,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://quiz-application-nq76.onrender.com/']
 
