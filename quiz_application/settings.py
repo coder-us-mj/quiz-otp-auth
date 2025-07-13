55f9f3c72f87c032c9f294f0d00c9921cf601b0a
aiqwip-vikas <vikas.patel@aiqwip.com>
2025-07-13 11:49:06 +0530

    AddedCORS

--- quiz_application/settings.py
+++ quiz_application/settings.py
@@ -16,6 +16,40 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quiz-application-nq76.onrender.com','quiz-otp-auth.onrender.com','*']
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1', 'localhost', 'quiz-application-nq76.onrender.com','quiz-otp-auth.onrender.com','*']
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3005",
+    "http://127.0.0.1:3005", 
+]
+
+CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 
 INSTALLED_APPS = [
